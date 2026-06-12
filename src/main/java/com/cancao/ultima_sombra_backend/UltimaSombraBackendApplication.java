90fdf4925b6c418c9da29a1db36e8174dd7954e4
Juliana Pelozo Pacheco <123774972+JulianaPelozo@users.noreply.github.com>
2026-06-12 15:02:31 -0300

    first commit

---
+++ src/main/java/com/cancao/ultima_sombra_backend/UltimaSombraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cancao.ultima_sombra_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimaSombraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltimaSombraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cancao/ultima_sombra_backend/UltimaSombraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cancao.ultima_sombra_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimaSombraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
